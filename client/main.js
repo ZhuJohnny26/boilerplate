e4ee74982c68a6e739b208a8abc1d1d0768857c8
ZhuJohnny26 <44298471+ZhuJohnny26@users.noreply.github.com>
2020-10-08 17:20:30 -0400

    react branch

---
+++ client/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            Boiling plates keeps them clean
+        </div>
+    )
+} 
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
