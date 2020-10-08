dba2ba267a152347a4f41ef2c2c6871068d6a573
ZhuJohnny26 <44298471+ZhuJohnny26@users.noreply.github.com>
2020-10-08 16:49:56 -0400

    removed console log

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-console.log('test',path.join(__dirname, '..', 'public', 'index.html'))
+
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use('*', (req,res) => {
